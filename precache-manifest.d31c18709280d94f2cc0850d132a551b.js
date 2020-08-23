848228b8892a07a0b8c45e0ff6e4ba7b84e3092a
Pawel Dabrowa <fox@Pawels-iMac-2.local>
2020-08-23 16:31:21 +0100

    Updates

---
+++ precache-manifest.d31c18709280d94f2cc0850d132a551b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2d8722f5f633c192de4a8a5b23a4038",
+    "url": "/fish-market-inventory-react-app/index.html"
+  },
+  {
+    "revision": "62c24d4a782de741d26f",
+    "url": "/fish-market-inventory-react-app/static/css/main.5767e00d.chunk.css"
+  },
+  {
+    "revision": "b83aa35f62cf7152a639",
+    "url": "/fish-market-inventory-react-app/static/js/2.9bbe138f.chunk.js"
+  },
+  {
+    "revision": "9b50f5741be789e4838182ccc7c0c6a5",
+    "url": "/fish-market-inventory-react-app/static/js/2.9bbe138f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62c24d4a782de741d26f",
+    "url": "/fish-market-inventory-react-app/static/js/main.c3d0748e.chunk.js"
+  },
+  {
+    "revision": "0274e69c8e4bf2d21db3",
+    "url": "/fish-market-inventory-react-app/static/js/runtime-main.0bf9ee3f.js"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/fish-market-inventory-react-app/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/fish-market-inventory-react-app/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/fish-market-inventory-react-app/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/fish-market-inventory-react-app/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/fish-market-inventory-react-app/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/fish-market-inventory-react-app/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/fish-market-inventory-react-app/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/fish-market-inventory-react-app/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/fish-market-inventory-react-app/static/media/haymaker-webfont.e9535f20.ttf"
+  }
+]);
